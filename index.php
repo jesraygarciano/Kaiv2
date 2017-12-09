d1083b968160f4290969d49e1860f154572f45c9
Jesray Garciano <jesray.garciano@gmail.com>
2017-12-09 12:41:38 +0800

    Made for adventure sirius

---
+++ index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Kai &mdash; Made for Adventure</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Kai made for adventure apparel" />
+	<meta name="keywords" content="Kai, Kai Apparel, Kai made for adventure, Adventure apparel, climb apparel, made for adventure, kai adventure apparel" />
+	<meta name="author" content="Jesray Garciano" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Merriweather:300,400|Montserrat:400,700|Roboto:900" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/icomoon.css">
+	<!-- Themify Icons-->
+	<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/themify-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/bootstrap.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/sass/stylesheets/screen.css">
+
+	<!-- Modernizr JS -->
+	<script src="<?php echo get_bloginfo('template_directory'); ?>/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="gtco-loader"></div>
+	
+	<div id="page">
+
+		<nav class="gtco-nav" role="navigation">
+			<div class="gtco-container">
+				
+				<div class="row">
+					<div class="col-sm-2 col-xs-12">
+						<div id="gtco-logo"><a href="index.html"><img src="<?php echo get_bloginfo('template_directory'); ?>/images/kai_white.png" /></a></div>
+					</div>
+					<div class="col-xs-10 text-right menu-1">
+						<ul>
+							<li class="active"><a href="index.html">Home</a></li>
+							<li><a href="portfolio.html">Featured</a></li>
+							<li class="has-dropdown">
+								<a href="services.html">Products</a>
+								<ul class="dropdown">
+									<li><a href="#">Souvenir Shirts</a></li>
+									<li><a href="#">ActVent</a></li>
+									<li><a href="#">Accessories</a></li>
+								</ul>
+							</li>
+							<li><a href="portfolio.html">Events</a></li>
+							<li><a href="portfolio.html">Gallery</a></li>
+							<li><a href="about.html">About</a></li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</nav>
+
+		<div class="gtco-container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="owl-carousel owl-carousel-fullwidth">
+						<div class="item">
+							<a href="#">
+								<img src="<?php echo get_bloginfo('template_directory'); ?>/images/hero_1.jpg" alt="Kai made for adventure apparel">
+								<div class="slider-copy">
+									<h2>Model #1</h2>
+								</div>
+							</a>
+						</div>
+						<div class="item">
+							<a href="#">
+								<img src="<?php echo get_bloginfo('template_directory'); ?>/images/hero_2.jpg" alt="Kai made for adventure apparel">
+								<div class="slider-copy">
+									<h2>Model #2</h2>
+								</div>
+							</a>
+						</div>
+						<div class="item">
+							<a href="#">
+								<img src="<?php echo get_bloginfo('template_directory'); ?>/images/hero_3.jpg" alt="Kai made for adventure apparel">
+								<div class="slider-copy">
+									<h2>Model 3 #1</h2>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="gtco-section">
+			<div class="gtco-container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 gtco-heading text-center">
+						<h2>WEAR YOUR PASSION FOR ADVENTURE</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat enim et urna sagittis, rhoncus euismod erat tincidunt. Donec tincidunt volutpat erat.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END Work -->
+
+		<div class="gtco-section">
+			<div class="gtco-container">
+				<div class="row">
+					<div class="col-md-6 gtco-news">
+						<h2>News</h2>	
+						<ul>
+							<li>
+								<img src="<?php echo get_bloginfo('template_directory'); ?>/images/event_2.jpg" />
+								<a href="#">
+									<span class="post-date">September 10, 2016</span>
+									<h3>Manila Bridge Re-construction</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat enim et urna sagittis, rhoncus euismod...</p>
+								</a>
+							</li>
+
+							<li>
+								<img src="<?php echo get_bloginfo('template_directory'); ?>/images/event_1.jpg" />
+								<a href="#">
+									<span class="post-date">September 10, 2016</span>
+									<h3>Manila Bridge Re-construction</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat enim et urna sagittis, rhoncus euismod...</p>
+								</a>
+							</li>
+
+							<li>
+								<img src="<?php echo get_bloginfo('template_directory'); ?>/images/event_3.jpg" />								
+								<a href="#">
+									<span class="post-date">September 10, 2016</span>
+									<h3>Manila Bridge Re-construction</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat enim et urna sagittis, rhoncus euismod...</p>
+								</a>
+							</li>
+						</ul>
+						<p><a href="#" class="btn btn-sm btn-special">More News</a></p>
+					</div>
+					<!-- END News -->
+					<div class="col-md-6 col-md-push-1 gtco-testimonials">
+						<h2>Testimonials</h2>
+								<div class="gtco-testimony">
+									<img src="<?php echo get_bloginfo('template_directory'); ?>/images/person_1.jpg" />
+								</div>
+								<blockquote>
+									<p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat enim et urna sagittis, rhoncus euismod erat tincidunt. Donec tincidunt volutpat erat.&ldquo;</p>
+									<p class="author"><cite>&mdash; John Doe Dueller</cite></p>
+								</blockquote>
+								<div class="gtco-testimony">
+									<img src="<?php echo get_bloginfo('template_directory'); ?>/images/person_2.jpg" />
+								</div>
+								<blockquote>
+									<p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat enim et urna sagittis, rhoncus euismod erat tincidunt. Donec tincidunt volutpat erat.&ldquo;</p>
+									<p class="author"><cite>&mdash; John Doe Dueller</cite></p>
+								</blockquote>
+								<div class="gtco-testimony">
+									<img src="<?php echo get_bloginfo('template_directory'); ?>/images/person_3.jpg" />
+								</div>
+								<blockquote>
+									<p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat enim et urna sagittis, rhoncus euismod erat tincidunt. Donec tincidunt volutpat erat.&ldquo;</p>
+									<p class="author"><cite>&mdash; John Doe Dueller</cite></p>
+								</blockquote>
+							</div>
+				</div>
+			</div>
+		</div>
+		<!-- END  -->
+
+<!-- 		<div class="gtco-section gto-features">
+			<div class="gtco-container">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="feature-left">
+							<i class="ti-zip icon"></i>
+							<div class="copy">
+								<h3>Architect</h3>
+								<p>Lorem ipsum dolor sit ameteista, consectetur adipiscing is not elitistaris.</p>
+								<p><a href="#" class="gtco-more">Learn more</a></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="feature-left">
+							<i class="ti-hummer icon"></i>
+							<div class="copy">
+								<h3>Planning</h3>
+								<p>Lorem ipsum dolor sit ameteista, consectetur adipiscing is not elitistaris.</p>
+								<p><a href="#" class="gtco-more">Learn more</a></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="feature-left">
+							<i class="ti-plug icon"></i>
+							<div class="copy">
+								<h3>Parks &amp; Events</h3>
+								<p>Lorem ipsum dolor sit ameteista, consectetur adipiscing is not elitistaris.</p>
+								<p><a href="#" class="gtco-more">Learn more</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> -->
+
+		<footer id="gtco-footer" class="gtco-section" role="contentinfo">
+			<div class="gtco-container">
+				<div class="row row-pb-md">
+					<div class="col-md-4 gtco-widget gtco-footer-paragraph">
+						<img src="<?php echo get_bloginfo('template_directory'); ?>/images/kai_white.png" />
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat enim et urna sagittis, rhoncus euismod.</p>
+					</div>
+					<div class="col-md-4">
+						<div class="row">
+							<div class="col-md-6 gtco-footer-link">
+								<h3>Sitemap</h3>
+								<ul class="gtco-list-link">
+									<li><a href="#">Home</a></li>
+									<li><a href="#">Features</a></li>
+									<li><a href="#">Products</a></li>
+									<li><a href="#">Testimonial</a></li>
+									<li><a href="#">Contact</a></li>
+								</ul>
+							</div>
+							<div class="col-md-6 gtco-footer-link">
+								<h3>Branches</h3>
+								<ul class="gtco-list-link">
+									<li><a href="#">Apas lahug</a></li>
+									<li><a href="#">Eagle Park</a></li>
+									<li><a href="#">Nationals Park</a></li>
+									<li><a href="#">Manila Park</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 gtco-footer-subscribe">
+						<form class="form-inline">
+						  <div class="form-group">
+						    <label class="sr-only" for="exampleInputEmail3">Email address</label>
+						    <input type="email" class="form-control" id="" placeholder="Email">
+						  </div>
+						  <button type="submit" class="btn btn-primary">Send</button>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="gtco-copyright">
+				<div class="gtco-container">
+					<div class="row">
+						<div class="col-md-6 text-left">
+							<p><small>&copy; 2016 Kai adventure apparel. All Rights Reserved. </small></p>
+						</div>
+						<div class="col-md-6 text-right">
+							<p>
+								<small>Powered by <a href="#" target="_blank">Jesray Garciano</a></small>
+							 </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- END footer -->
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="<?php echo get_bloginfo('template_directory'); ?>/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="<?php echo get_bloginfo('template_directory'); ?>/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="<?php echo get_bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="<?php echo get_bloginfo('template_directory'); ?>/js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="<?php echo get_bloginfo('template_directory'); ?>/js/owl.carousel.min.js"></script>
+
+	<!-- Main -->
+	<script src="<?php echo get_bloginfo('template_directory'); ?>/js/main.js"></script>
+
+	</body>
+</html>
+
