06fe51f5e47d5636890a343a7fe90832764f105e
Jesray Garciano <jesray.garciano@gmail.com>
2018-01-01 12:30:44 +0800

    January 2018 first day

--- page-about.php
+++ page-about.php
@@ -1,18 +1,20 @@
 <?php get_header(); ?>
 	
 	<div class="container aboutpage">
+
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-5">
 				<img src="<?php echo get_bloginfo('template_directory'); ?>/images/company_logo.png" alt="Kai made for adventure logo">
 			</div>
-			<div class="col-md-6 ">
+
+			<div class="col-md-5 about-context">
 				<h2>Company Profile</h2>
 				<p>
 				Kai Adventure Wear is a proud Philippine brand that specializes in adventure souvenirs for outdoor enthusiasts alike. it also provides customized tshirts souvenirs for outdoor events and activities.
 
 				Our goal is to provide all adventure enthusiasts with top quality outdoor souvenirs to suit their needs.
-				</p>
-			</div>			
+				</p>				
+			</div>
 		</div>
 	</div>
 <?php get_footer(); ?>
--- page-contact.php
+++ page-contact.php
@@ -1,21 +1,52 @@
 <?php get_header(); ?>
 	
-	<div class="container aboutpage">
+	<div class="container contactpage">
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-5">
 				<img src="<?php echo get_bloginfo('template_directory'); ?>/images/company_logo.png" alt="Kai made for adventure logo">
 			</div>
-			<div class="col-md-6 ">
-				<h2>Contact Us</h2>
-				<p>
-					<ul>
-						<li>Address:</li>
-						<li>Facebook page:</li>
-						<li>Instagram:</li>
-						<li>Email:</li>																		
-					</ul>
-				</p>
-			</div>			
+
+			<div class="col-md-5 contact-context">
+				<h2>Contact</h2>
+				<div class="row">
+					<div class="col-xs-4">
+						ADDRESS:
+					</div>
+					<div class="col-xs-8">
+						1795 San Miguel Apas, Cebu City Philippines 6000
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-xs-4">
+						FACEBOOK:
+					</div>
+					<div class="col-xs-8">
+						<a href="https://www.facebook.com/kaiadventurewear">
+							www.facebook.com/kaiadventurewear
+						</a>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-xs-4">
+						INSTAGRAM:
+					</div>
+					<div class="col-xs-8">
+						kai.adventures
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-xs-4">
+						EMAIL:
+					</div>
+					<div class="col-xs-8">
+						kaiadventureapparel@gmail.com
+					</div>
+				</div>
+			</div>
+
 		</div>
 	</div>
 <?php get_footer(); ?>
