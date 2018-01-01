f757f7bb4791bb369d79870b754f8ad55b9cac0e
Jesray Garciano <jesray.garciano@gmail.com>
2018-01-01 11:09:51 +0800

    January 2018 first day

--- page-actvent.php
+++ page-actvent.php
@@ -3,7 +3,9 @@
 <div class="container">
 	
 	<h1>Actvent page page</h1>
-	<?php echo do_shortcode("[huge_it_catalog id='9']"); ?>
+		<div class="animate-box">
+		<?php echo do_shortcode("[huge_it_catalog id='9']"); ?>
+		</div>
 </div>
 
 <?php get_footer(); ?>
