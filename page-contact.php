87c050ce96aa3250396160f80a3439840a63e76c
Jesray Garciano <jesray.garciano@gmail.com>
2018-02-03 14:54:42 +0800

    Fix: Contact page

--- page-contact.php
+++ page-contact.php
@@ -1,46 +1,46 @@
-<?php get_header(); ?>
-	
-	<div class="container contactpage">
-		<div class="row">
-			<div class="col-md-2">
-			</div>
-			<div class="col-md-4">
-				<img src="<?php echo get_bloginfo('template_directory'); ?>/images/company_logo.png" alt="Kai made for adventure logo">
-			</div>
-
-			<div class="col-md-6 contact-context">
-				<h2>Contact</h2>
-
+		<?php get_header(); ?>
+			
+			<div class="container contactpage">
 				<div class="row">
-					<div class="col-xs-3">
-						FACEBOOK:
+					<div class="col-md-2">
 					</div>
-					<div class="col-xs-8">
-						<a href="https://www.facebook.com/kaiadventurewear">
-							www.facebook.com/kaiadventurewear
-						</a>
+					<div class="col-md-4">
+						<img src="<?php echo get_bloginfo('template_directory'); ?>/images/company_logo.png" alt="Kai made for adventure logo">
 					</div>
-				</div>
 
-				<div class="row">
-					<div class="col-xs-3">
-						INSTAGRAM:
-					</div>
-					<div class="col-xs-8">
-						kai.adventures
-					</div>
-				</div>
+					<div class="col-md-6 contact-context">
+						<h2>Contact</h2>
 
-				<div class="row">
-					<div class="col-xs-3">
-						EMAIL:
+						<div class="row">
+							<div class="col-xs-3">
+								FACEBOOK:
+							</div>
+							<div class="col-xs-8">
+								<a href="https://www.facebook.com/kaiadventurewear">
+									www.facebook.com/kaiadventurewear
+								</a>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-xs-3">
+								INSTAGRAM:
+							</div>
+							<div class="col-xs-8">
+								kai.adventures
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-xs-3">
+								EMAIL:
+							</div>
+							<div class="col-xs-8">
+								kaiadventureapparel@gmail.com
+							</div>
+						</div>
 					</div>
-					<div class="col-xs-8">
-						kaiadventureapparel@gmail.com
-					</div>
+
 				</div>
 			</div>
-
-		</div>
-	</div>
-<?php get_footer(); ?>
+		<?php get_footer(); ?>
