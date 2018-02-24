dddf8d269f17ae84f69ebb55f5ca232911969e10
Jesray Garciano <jesray.garciano@gmail.com>
2018-02-24 16:39:45 +0800

    minor changes

--- index.php
+++ index.php
@@ -1,53 +1,53 @@
-<?php get_header(); ?>
+    <?php get_header(); ?>
 
-<div class="container">  
-    <div class="row">
+    <div class="container">  
+        <div class="row">
 
-      <div class="col-sm-8 blog-main">
-      	<?php if(have_posts()) : ?>
-      		<?php while(have_posts()) : the_post(); ?>
-	          <div class="blog-post animate-box">
-	            <h2 class="blog-post-title animate-box">
-	            	<a href="<?php the_permalink(); ?>">
-	            	<?php the_title(); ?>
-            		</a>
-            	</h2>
-	            <p class="blog-post-meta animate-box">
-	            	<?php the_time('F j, Y g:i a'); ?>
-	            	by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
-	            		<?php the_author(); ?>
-	            	   </a>
-            	</p>
-            	<?php if(has_post_thumbnail()) : ?>
-            		<div class="post-thumb">
-	            		<?php the_post_thumbnail(); ?>
-            		</div>
-          		<?php endif; ?>
+          <div class="col-sm-8 blog-main">
+          	<?php if(have_posts()) : ?>
+          		<?php while(have_posts()) : the_post(); ?>
+    	          <div class="blog-post animate-box">
+    	            <h2 class="blog-post-title animate-box">
+    	            	<a href="<?php the_permalink(); ?>">
+    	            	<?php the_title(); ?>
+                		</a>
+                	</h2>
+    	            <p class="blog-post-meta animate-box">
+    	            	<?php the_time('F j, Y g:i a'); ?>
+    	            	by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+    	            		<?php the_author(); ?>
+    	            	   </a>
+                	</p>
+                	<?php if(has_post_thumbnail()) : ?>
+                		<div class="post-thumb">
+    	            		<?php the_post_thumbnail(); ?>
+                		</div>
+              		<?php endif; ?>
 
-	            <?php the_excerpt(); ?>
+    	            <?php the_excerpt(); ?>
 
-              <p><a href="<?php the_permalink(); ?>" class="btn btn-sm btn-special text-right animate-box">Read more</a></p>
+                  <p><a href="<?php the_permalink(); ?>" class="btn btn-sm btn-special text-right animate-box">Read more</a></p>
 
-	          </div><!-- /.blog-post -->
-    	<?php endwhile; ?>
-      <?php else : ?>
-      	<p><?php __('No Posts Found'); ?></p>
-      <?php endif; ?>
+    	          </div><!-- /.blog-post -->
+        	<?php endwhile; ?>
+          <?php else : ?>
+          	<p><?php __('No Posts Found'); ?></p>
+          <?php endif; ?>
 
-        </div><!-- /.blog-main -->
+            </div><!-- /.blog-main -->
 
-        <div class="col-sm-3 offset-sm-1 blog-sidebar">
-          <div class="sidebar-module sidebar-module-inset">
-            <h4 class="animate-box">Keep updated in every Kai events</h4>
-          </div>
-          <?php if(is_active_sidebar('sidebar')): ?>
-            <?php wp_list_categories('title_li='); ?>
+            <div class="col-sm-3 offset-sm-1 blog-sidebar">
+              <div class="sidebar-module sidebar-module-inset">
+                <h4 class="animate-box">Keep updated in every Kai events</h4>
+              </div>
+              <?php if(is_active_sidebar('sidebar')): ?>
+                <?php wp_list_categories('title_li='); ?>
 
-            <!-- <?php dynamic_sidebar('sidebar'); ?> -->
-        <?php endif; ?>
-        </div><!-- /.blog-sidebar -->
+                <!-- <?php dynamic_sidebar('sidebar'); ?> -->
+            <?php endif; ?>
+            </div><!-- /.blog-sidebar -->
 
-      </div><!-- /.row -->
-</div> <!-- /.container -->
+          </div><!-- /.row -->
+    </div> <!-- /.container -->
 
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
