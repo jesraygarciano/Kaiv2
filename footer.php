195a74468f10362dd1a8eb056c966e7643e196fe
Jesray Garciano <jesray.garciano@gmail.com>
2018-05-06 03:03:38 +0800

    Modification footer

--- footer.php
+++ footer.php
@@ -1,76 +1,76 @@
-		<footer id="gtco-footer" class="gtco-section" role="contentinfo">
-			<div class="gtco-container">
-				<div class="row row-pb-md">
-					<div class="col-md-4 gtco-widget gtco-footer-paragraph">
-						<img src="<?php echo get_bloginfo('template_directory'); ?>/images/nav_logo.png" />
-						<p>Kai Adventure Wear is an outdoor brand apparel that specializes in adventure souvenirs for outdoor enthusiasts alike.</p>
-					</div>
-					<div class="col-md-8">
-						<div class="row">
-							<div class="col-md-4 gtco-footer-link">
-								<h3>Sitemap</h3>
-								<ul class="gtco-list-link">
-									<li><a href="http://www.kaiadventurewear.com/">Home</a></li>
-									<li><a href="http://www.kaiadventurewear.com/events/">Events</a></li>
-									<li><a href="http://www.kaiadventurewear.com/featured/">Features</a></li>
-									<li><a href="http://www.kaiadventurewear.com/about/">About Us</a></li>
-									<li><a href="http://www.kaiadventurewear.com/contact/">Contact Us</a></li>
-								</ul>
-							</div>
-							<div class="col-md-4 gtco-footer-link">
-								<h3>Products</h3>
-								<ul class="gtco-list-link">
-									<li><a href="http://www.kaiadventurewear.com/products/souvenir/">Souvenirs</a></li>
-									<li><a href="http://www.kaiadventurewear.com/products/actvent/">Actvent</a></li>
-									<li><a href="http://www.kaiadventurewear.com/products/accessories/">Accessories</a></li>
-								</ul>
-							</div>
-							<div class="col-md-4 gtco-footer-link">
-								<h3>Testimonials</h3>
-								<ul class="gtco-list-link">
-									<li><a href="https://web.facebook.com/pg/kaiadventurewear/reviews/" target="_blank">Testimonials</a></li>
-								</ul>
-							</div>							
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="gtco-copyright">
+			<footer id="gtco-footer" class="gtco-section" role="contentinfo">
 				<div class="gtco-container">
-					<div class="row">
-						<div class="col-md-6 text-left">
-							<p><small>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></small></p>
+					<div class="row row-pb-md">
+						<div class="col-md-4 gtco-widget gtco-footer-paragraph">
+							<img src="<?php echo get_bloginfo('template_directory'); ?>/images/nav_logo.png" />
+							<p>Kai Adventure Wear is an outdoor brand apparel that specializes in adventure souvenirs for outdoor enthusiasts alike.</p>
 						</div>
-						<div class="col-md-6 text-right">
-							<p>
-								<small>Powered by <a href="#" target="_blank">Kai</a></small>
-							 </p>
+						<div class="col-md-8">
+							<div class="row">
+								<div class="col-md-4 gtco-footer-link">
+									<h3>Sitemap</h3>
+									<ul class="gtco-list-link">
+										<li><a href="http://www.kaiadventurewear.com/">Home</a></li>
+										<li><a href="http://www.kaiadventurewear.com/events/">Events</a></li>
+										<li><a href="http://www.kaiadventurewear.com/featured/">Features</a></li>
+										<li><a href="http://www.kaiadventurewear.com/about/">About Us</a></li>
+										<li><a href="http://www.kaiadventurewear.com/contact/">Contact Us</a></li>
+									</ul>
+								</div>
+								<div class="col-md-4 gtco-footer-link">
+									<h3>Products</h3>
+									<ul class="gtco-list-link">
+										<li><a href="http://www.kaiadventurewear.com/products/souvenir/">Souvenirs</a></li>
+										<li><a href="http://www.kaiadventurewear.com/products/actvent/">Actvent</a></li>
+										<li><a href="http://www.kaiadventurewear.com/products/accessories/">Accessories</a></li>
+									</ul>
+								</div>
+								<div class="col-md-4 gtco-footer-link">
+									<h3>Testimonials</h3>
+									<ul class="gtco-list-link">
+										<li><a href="https://web.facebook.com/pg/kaiadventurewear/reviews/" target="_blank">Testimonials</a></li>
+									</ul>
+								</div>							
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</footer>
-		<!-- END footer -->
+				<div class="gtco-copyright">
+					<div class="gtco-container">
+						<div class="row">
+							<div class="col-md-6 text-left">
+								<p><small>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></small></p>
+							</div>
+							<div class="col-md-6 text-right">
+								<p>
+									<small>Powered by <a href="#" target="_blank">Kai</a></small>
+								 </p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+			<!-- END footer -->
 
-	</div>
-	<div class="gototop js-top">
-		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
-	</div>
-	
-	<!-- jQuery -->
-	<script src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="<?php bloginfo('template_url'); ?>/js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="<?php bloginfo('template_url'); ?>/js/jquery.waypoints.min.js"></script>
-	<!-- Carousel -->
-	<script src="<?php bloginfo('template_url'); ?>/js/owl.carousel.min.js"></script>
+		</div>
+		<div class="gototop js-top">
+			<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+		</div>
+		
+		<!-- jQuery -->
+		<script src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script>
+		<!-- jQuery Easing -->
+		<script src="<?php bloginfo('template_url'); ?>/js/jquery.easing.1.3.js"></script>
+		<!-- Bootstrap -->
+		<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+		<!-- Waypoints -->
+		<script src="<?php bloginfo('template_url'); ?>/js/jquery.waypoints.min.js"></script>
+		<!-- Carousel -->
+		<script src="<?php bloginfo('template_url'); ?>/js/owl.carousel.min.js"></script>
 
-	<!-- Main -->
-	<script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
-	
-	<?php wp_footer(); ?> 
-	</body>
-</html>
+		<!-- Main -->
+		<script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+		
+		<?php wp_footer(); ?> 
+		</body>
+	</html>
